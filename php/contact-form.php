ecf152844b1f9ed1524a7e7d80513c66ff4aedca
Sebastián Ortega <sortegapizarro@gmail.com>
2019-02-05 13:52:05 -0300

    Cambios varios
    Se añade el horario del domingo.
    Se añade formulario de contacto.
    Correciones ortograficas

--- php/contact-form.php
+++ php/contact-form.php
@@ -4,38 +4,38 @@ if(isset($_POST["action"])) {
   $email = $_POST['email'];     // Sender's email address
   $phone  = $_POST['phone'];     // Sender's email address
   $message = $_POST['message'];    // Sender's message
-  $from = 'Demo Contact Form';    
-  $to = 'demo@domin.com';     // Recipient's email address
-  $subject = 'Message from Contact Demo ';
+  $from = 'contacto@patiperrosrestaurante.cl';    
+  $to = 'contacto@patiperrosrestaurante.cl';     // Recipient's email address
+  $subject = 'Contacto desde el sitio web ';
 
- $body = " From: $name \n E-Mail: $email \n Phone : $phone \n Message : $message"  ;
+ $body = " De: $name \n E-Mail: $email \n Telefono : $phone \n Mensaje : $message"  ;
 	
 	// init error message 
 	$errmsg='';
   // Check if name has been entered
   if (!$_POST['name']) {
-   $errmsg = 'Please enter your name';
+   $errmsg = 'Por favor ingrese su nombre';
   }
 
   
   // Check if email has been entered and is valid
   if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-   $errmsg = 'Please enter a valid email address';
+   $errmsg = 'Por favor ingrese un correo electrónico valido';
   }
   
   //Check if message has been entered
   if (!$_POST['message']) {
-   $errmsg = 'Please enter your message';
+   $errmsg = 'Por favor ingrese su mensaje';
   }
  
 	$result='';
   // If there are no errors, send the email
   if (!$errmsg) {
 		if (mail ($to, $subject, $body, $from)) {
-			$result='<div class="alert alert-success">Thank you for contacting us. Your message has been successfully sent. We will contact you very soon!</div>'; 
+			$result='<div class="alert alert-success">Gracias por contactarnos. Su mensaje ha sido enviado con éxito. Nos pondremos en contacto con usted muy pronto!</div>'; 
 		} 
 		else {
-		  $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+		  $result='<div class="alert alert-danger">Lo siento, hubo un error al enviar tu mensaje. Por favor, inténtelo de nuevo más tarde.</div>';
 		}
 	}
 	else{
